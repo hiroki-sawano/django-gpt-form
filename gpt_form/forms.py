59cc65683fe5b3200bb12649231959f5bc0df712
hiroki-sawano <hiroki.sawano.2512@gmail.com>
2023-04-28 22:09:24 +0900

    Use Form.Media to manage CSS

--- gpt_form/forms.py
+++ gpt_form/forms.py
@@ -3,3 +3,8 @@ from django import forms
 
 class CompletionForm(forms.Form):
     prompt = forms.CharField(label='Prompt', widget=forms.Textarea)
+
+    class Media:
+        css = {
+            'all': ['gpt_form/css/completion-form.css']
+        }
